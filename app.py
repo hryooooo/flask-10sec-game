927039878a4bae4c6efc87dce9c78628eff2c0d0
hryooooo <h.ryo_0113@icloud.com>
2025-02-22 11:42:06 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
+import eventlet
+eventlet.monkey_patch()
+
 from flask import Flask, render_template, request
 from flask_socketio import SocketIO, emit
 import time
-import eventlet
-
-eventlet.monkey_patch()
 
 app = Flask(__name__)
 socketio = SocketIO(app, cors_allowed_origins="*", async_mode="eventlet")
